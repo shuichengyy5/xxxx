128abf930b2a384f10914bb10c823b4b5ff616de
shuichengyy5 <3331592296@qq.com>
2025-02-28 03:46:27 +0800

    2_22222.py

--- 1_main.py
+++ 1_main.py
@@ -1,4 +1,15 @@
-#
-#
-#
-print('Hello World')
+#202410111303
+#333159226@qq.com
+#余洋
+def calculate_sum(nums):
+    while len(nums)>1:
+        new_nums=[(nums[i] + nums[i+1]) % 10 for i in range(len(nums)-1)]
+        nums=new_nums
+    return nums[0]
+
+input_nums=input("")
+nums=list(map(int,input_nums.split(',')))
+
+result=calculate_sum(nums)
+
+print(result)
